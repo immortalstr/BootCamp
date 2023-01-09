8adcf1618632df99a418544c8a03fb5196f29d41
immortalstr <immortalstr1990@mail.ru>
2023-01-09 14:04:54 +0300

    add Code

---
+++ Task001/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+int size = 1000000;
+
+int m = 30000;
+
+
+int[] array = Enumerable.Range(1, size)
+                        .Select(item => Random.Shared.Next(10))
+                        .ToArray();
+//Console.WriteLine($"[{string.Join(", ", array)}]");
+
+Stopwatch sw = new();
+sw.Start();
+
+int max = 0;
+for (int i = 0; i < array.Length - m; i++)
+{
+    int t = 0;
+    for (int j = i; j < i + m; j++)
+    {
+        t += array[j];
+    }
+    if (t>max) max = t;
+}
+sw.Stop();
+
+Console.WriteLine($"time = {sw.ElapsedMilliseconds}");
+Console.WriteLine(max);
