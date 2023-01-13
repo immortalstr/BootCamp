762d07c3f23c37c1e23b025aa79b607b2cad57ab
immortalstr <immortalstr1990@mail.ru>
2023-01-13 16:53:45 +0300

    Добавлены 2 модуля вспомогательных

--- Task002/Program.cs
+++ Task002/Program.cs
@@ -1,22 +1,7 @@
-﻿void SortSelection(int[] collection)
-{
-    int size = collection.Length;
+﻿using static Sorting;
+using static Infrastructure;
 
-    for (int i = 0; i < size - 1; i++)
-    {
-        int pos = i;
-        for (int j = i + 1; j < size; j++)
-        {
-            if (collection[j] < collection[pos]) pos = j;
-        }
-        int temp = collection[i];
-        collection[i] = collection[pos];
-        collection[pos] = temp;
-    }
-}
-
-int[] array = {1,2,3,5,3,4,7,3,8};
-
-Console.WriteLine($"[{String.Join(',', array)}]");
+int[] array = { 1, 2, 3, 5, 3, 4, 7, 3, 8 };
+Show(array);
 SortSelection(array);
-Console.WriteLine($"[{String.Join(',', array)}]");
+Show(array);
