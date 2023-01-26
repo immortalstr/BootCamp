445eec773480752326077e2c5f436a2e43c62507
immortalstr <immortalstr1990@mail.ru>
2023-01-26 14:36:30 +0300

    add opencv video and picture

--- Opencv/opencv.py
+++ Opencv/opencv.py
@@ -1,4 +1,9 @@
 import cv2
 
-img = cv2.imread("test.jpg")
-print(img)
+img = cv2.imread("test.png")
+print(img.shape)
+img = cv2.resize(img, (10000, 10000))
+print(img.shape)
+cv2.imshow("Result", img)
+
+cv2.waitKey(0)
